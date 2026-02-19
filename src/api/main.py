73150e64f87980d637b35d39ef52c54f4296a044
MosNanthaphop <nanthaphop.weerahong@gmail.com>
2026-02-19 17:44:21 +0700

    add new models backend

--- src/api/main.py
+++ src/api/main.py
@@ -62,7 +62,7 @@ model = create_model(num_classes=len(CLASS_NAMES))
 
 # หา path ของ model
 BASE_DIR = Path(__file__).resolve().parent
-model_path = BASE_DIR.parent.parent / "model" / "efficientnet_b2.pth"
+model_path = BASE_DIR.parent.parent / "model" / "final.pth"
 
 # ตรวจสอบว่าไฟล์มีจริง
 if not model_path.exists():
