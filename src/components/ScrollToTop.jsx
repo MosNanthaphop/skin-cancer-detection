6f2bdd52869073a484a453397aeed8b2123f651f
MosNanthaphop <nanthaphop.weerahong@gmail.com>
2026-03-06 20:56:13 +0700

    check update on vercel

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -3,15 +3,13 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  // ดึง pathname มาเพื่อจับตามองว่าเปลี่ยนหน้าหรือยัง
   const { pathname } = useLocation();
 
   useEffect(() => {
-    // เมื่อ pathname เปลี่ยน ให้ scroll ไปที่บนสุด (0, 0)
     window.scrollTo(0, 0);
-  }, [pathname]); // ทำงานทุกครั้งที่ pathname เปลี่ยน
+  }, [pathname]);
 
-  return null; // Component นี้ไม่ต้องแสดงผลอะไร
+  return null;
 };
 
 export default ScrollToTop;
